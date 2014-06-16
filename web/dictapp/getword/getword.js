cb848ed84ca7197ee56c90c5a84dc055f434edca
liuhongjiang <andrew.lhj@gmail.com>
2014-06-16 22:45:53 +0800

    finished index.html

--- web/dictapp/getword/getword.js
+++ web/dictapp/getword/getword.js
@@ -26,22 +26,40 @@ exports.getAndDivide = function(req, res) {
             // var gbk_to_utf8_iconv = new Iconv('GBK', 'UTF-8//TRANSLIT//IGNORE');
             // var utf8_buffer = gbk_to_utf8_iconv.convert(buffer);
             // console.log(buffer.toString());
+            var retObj = {};
+
             var htmlText = buffer.toString();
+            var errStart = htmlText.indexOf('请查看输入的字词是否有错误');
+            if (errStart > 0) {
+                res.end(JSON.stringify({"error": "This is not a word!"}));
+                return;
+            }
+
             var start = htmlText.indexOf('<div class="trans-container">');
             var end = htmlText.indexOf('</div>', start);
-            var translation = htmlText.substring(start + '<div class="trans-container">'.length, end);
-            console.log(translation);
+            if (start > 0) {
+                var translation = htmlText.substring(start + '<div class="trans-container">'.length, end);
+                console.log(translation);
+                retObj["translation"] = translation;
+            }
 
             var pstart = htmlText.indexOf('<span class="phonetic">');
             pstart = htmlText.indexOf('<span class="phonetic">', pstart + '<span class="phonetic">'.length);
             var pend = htmlText.indexOf('</span>', pstart);
        
-            var pronounce = htmlText.substring(pstart + '<span class="phonetic">'.length, pend)
-            console.log("pronounce: " + pronounce);
-            parts = divide.divide(pronounce.substring(1, pronounce.length - 1 ))
-            console.log(parts);
-            var dividedPronounce = parts.join("-");
-            res.end(JSON.stringify({"translation": translation, "pronounce": pronounce, "dividedPronounce": dividedPronounce}));
+            if (pstart > 0) {
+                var pronounce = htmlText.substring(pstart + '<span class="phonetic">'.length, pend)
+                console.log("pronounce: " + pronounce);
+                parts = divide.divide(pronounce.substring(1, pronounce.length - 1 ))
+                console.log(parts);
+                var dividedPronounce = parts.join("-");
+                retObj["pronounce"] = pronounce;
+                retObj["dividedPronounce"] = dividedPronounce;
+            } else {
+                retObj["pronounce"] = "";
+                retObj["dividedPronounce"] = "";
+            }
+            res.end(JSON.stringify(retObj));
             // for (var i = 0; i < pronounce.length; i++) {
             //     console.log(divide.isyy(pronounce[i]));
             // };
